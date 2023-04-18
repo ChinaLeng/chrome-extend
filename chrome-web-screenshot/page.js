499ca32df59ee24e35d6900554d5846212ba017c
chenzhengkuan <893579684@qq.com>
2023-04-18 17:42:35 +0800

    update

--- chrome-web-screenshot/page.js
+++ chrome-web-screenshot/page.js
@@ -11,9 +11,84 @@ let initialX,
 //需要滚动的距离
 let residueX,
     residueY;
+    
+let arrangements = [];
 
     chrome.runtime.onMessage.addListener(async function(request, sender,callback) {
-        console.log("request",request);
-        callback();
+        if (request.msg === 'scrollPage') {
+            initScroll();
+            initInner();
+            callback();
+            geyCountCoordinate();
+            getScreenshot();
+        }else{
+            getScreenshot();
+        }
+        
         return true;
     });
+
+function initialization(){
+    initialX = window.scrollX, 
+    initialY = window.scrollY,
+    initialOverflow = document.documentElement.style.overflow;
+}
+
+function initScroll(){
+    scrollWidth = document.documentElement.scrollWidth,
+    scrollHeight = document.documentElement.scrollHeight;
+}
+
+function initInner(){
+    innerWidth = window.innerWidth,
+    innerHeight = window.innerHeight;
+}
+
+function overflowHidden(){
+    //把滚动条隐藏，截图的图片不需要滚动条
+    document.documentElement.style.overflow = 'hidden';
+}
+
+function geyCountCoordinate(){
+    //剩余需要滚动的高度 
+    let residueY = scrollHeight - innerHeight;
+    let residueX;
+    //y轴页面滚动到最底层时 停止
+    while(residueY > -innerHeight){
+        residueX = 0;
+        //x轴页面也需要滚动
+        while(residueX < scrollWidth){
+            arrangements.push([residueX, residueY]);
+            residueX += innerWidth;
+        }
+        //减去滚动的高度
+        residueY -= innerHeight;
+    }
+    console.log("arrangements",arrangements);
+}
+
+function getScreenshot(){
+    if(arrangements.length == 0){
+        chrome.runtime.sendMessage({msg:"endPage"});
+        return;
+    }
+    
+    let next = arrangements.shift();
+    let x = next[0], y = next[1];
+    window.scrollTo(x, y);
+
+    let data = {
+        msg: 'capturePage',
+        x: window.scrollX,
+        y: window.scrollY,
+        complete: 3,
+        totalWidth: scrollWidth,
+        totalHeight: scrollHeight,
+        devicePixelRatio: window.devicePixelRatio //当前显示设备的物理像素分辨率与CSS 像素分辨率之比
+    };
+
+    setTimeout(() => {
+        // 将滚动后获取的网页的可视位置坐标数据发送消息给popup.js
+        chrome.runtime.sendMessage(data);
+    }, 550)
+}
--- chrome-web-screenshot/popup.js
+++ chrome-web-screenshot/popup.js
@@ -1,13 +1,3 @@
-//注入文件是否成功标识
-let initial = false;
-//当点击popup页面时  注入js和css文件
-// chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
-//     let tab = tabs[0];
-//     chrome.scripting.executeScript({target: {tabId: tab.id} , files: ['page.js'],}, function() {
-//         sendScrollMessage(tab); // 发送消息通知
-//     });
-// });
-
 window.onload=function(){
     document.getElementById("entire-web").addEventListener("click", function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -23,6 +13,56 @@ function sendScrollMessage(tab) {
     chrome.tabs.sendMessage(tab.id, {msg: 'scrollPage'}, function() {
         // 发送后的回调函数
         console.log("after send scrollPage message")
+    });
+}
+
+function sendScrollMessage1(tab) {
+    chrome.tabs.sendMessage(tab.id, {msg: 'continuePage'}, function() {
+        // 发送后的回调函数
+        console.log("after send continuePage message")
+    });
+}
+
+chrome.runtime.onMessage.addListener(async function(request, sender) {
+    console.log("request",request);
+    if (request.msg === 'capturePage') {
+        await capturePage(request,sender);
+    }else{
+
+    }
+    
+});
+async function capturePage(data, sender, sendResponse) {
+    return new Promise((resolve, reject) => {
+    var screenshot = {};
+    var canvas;
+    if (!screenshot.canvas) {
+        canvas = document.createElement('canvas');
+        canvas.width = data.totalWidth;
+        canvas.height = data.totalHeight;
+        screenshot.canvas = canvas;
+        screenshot.ctx = canvas.getContext('2d');
+    }
+
+    chrome.tabs.captureVisibleTab(
+        null, {format: 'png', quality: 100}, function(dataURI) {
+            console.log("dataURI",dataURI);
+            if (dataURI) {
+                var image = new Image();
+                image.onload = function() {
+                    console.log('draw img postions: ' + data.x + ', ' + data.y);
+                    screenshot.ctx.drawImage(image, data.x, data.y);// 将当前片段图片放到相应位置
+                    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
+                        let tab = tabs[0]
+                        sendScrollMessage1(tab);
+                        resolve(); 
+                    }); 
+                };
+                image.src = dataURI;
+            }
+        });
+
+        console.log("screenshot.canvas",screenshot.canvas);
     });
 }
 
